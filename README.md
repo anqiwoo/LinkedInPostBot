b70204d03de4f903b8055c40bb44b22f01eeac13
anqiwoo <anqiwu85@outlook.com>
2021-12-29 20:21:45 +0800

    21/12/28 Possibly Last Commit

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # LinkedInPostBot
+
+## Function of the main.py script:
+
+- Get your LinkedIn email/username and password from 'config.txt', which you have to set up on your own.
+- Open up a Chrome web page and direct to 'https://www.linkedin.com'.
+- Auto Login into your LinkedIn account and open the Create a Post View.
+
+## Some Thoughts about the project:
+
+My initial drive of building this project is to automate my everyday LinkedIn post.
+After around 4 hours of building this project, I would recommend you search for LinkedIn API resources if you want to realize the automation of your LinkedIn post and have some admin access to a LinkedIn Company Page.
+
+Since I am an independent developer and I am okay with the present posting process(after testing my script for hours lol), I'd like to end this project right here.
+
+Thanks for reading.
--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from selenium import webdriver
-import time
 
 
 if __name__ == '__main__':
@@ -9,12 +8,6 @@ if __name__ == '__main__':
 
     browser = webdriver.Chrome('chromedriver.exe')
 
-    # elementID = browser.find_element_by_id('username')
-    # elementID.send_keys(USERNAME)
-    # elementID = browser.find_element_by_id('password')
-    # elementID.send_keys(PASSWORD)
-    # elementID.submit()
-
     # Login to linkedin
     browser.get('https://www.linkedin.com/login')
     elem = browser.find_element_by_name("session_key")
@@ -22,32 +15,6 @@ if __name__ == '__main__':
     elem = browser.find_element_by_name("session_password")
     elem.send_keys(PASSWORD)
     elem.submit()
-    # c = browser.find_element_by_id("login-submit")
-    # c.click()
 
-    # Learn how to add arguments to start this script in command line
-    # text = 'For LinkinedIn Bot Test.'
-
-    # browser.find_element_by_class_name("ql-editor.ql-blank").send_keys(text)
-    # time.sleep(1)
-    # browser.find_element_by_id('ember500').click()
+    # Direct to the Create a Post View
     browser.find_element_by_id('ember44').click()
-    # browser.find_element_by_class_name(
-    #     'share-creation-state__text-editor').send_keys(text)
-    # browser.find_element_by_id("ember500")
-
-    # post_box = browser.find_element_by_class_name(
-    #     "sharing-create-share-view__create-content")
-    # post_box.click()
-    # sleep(2)
-    # mes = browser.find_element_by_class_name(
-    #     "mentions-texteditor__contenteditable")
-    # mes.send_keys(message)
-    # sleep(2)
-    # post_box = browser.find_element_by_xpath(
-    #     "//*[@data-control-name='share.post']")
-    # post_box.click()
-    # sleep(2)
-    # print('Posted!')
-    # browser.quit()
-    # print('End!')
