22e4e3e6c3ea3c189c1fe179f8d5dd2693ca5885
anqiwoo <anqiwu85@outlook.com>
2022-01-05 22:31:32 +0800

    Change on 22/1/5

--- main.py
+++ main.py
@@ -5,14 +5,41 @@ from PIL import Image
 # Depending on your need, you can uncomment the last few lines of codes to save the screenshot of your today's post and open it for checking purpose.
 
 
+def post(text):
+    # Direct to the Create a Post View
+    t.sleep(2)
+    browser.find_element_by_id('ember35').click()
+
+    # Enter the Post Content
+    t.sleep(2)
+    postarea = browser.find_element_by_class_name("ql-editor.ql-blank")
+    postarea.send_keys(text)
+
+    # Press the Post Button
+    t.sleep(2)
+    browser.find_element_by_xpath(
+        "//div[@class='share-box_actions']//span[@class='artdeco-button__text']").click()
+
+    # # Save the Screenshot (Just for checking the post)
+    t.sleep(2)
+    # browser.save_screenshot("TodayPost.png")
+    print('\n\nMade one Post and Everything went well! Hooray!')
+
+    # # Open up the png file for checking
+    # img = Image.open('TodayPost.png')
+    # img.show()
+
+
 if __name__ == '__main__':
     # Open the post.txt for you to edit the post content.
     os.startfile('post.txt')
+    os.startfile('read.txt')
     t.sleep(120)  # Leave 2 mins to check the post content.
-    with open('config.txt') as f1, open('post.txt', encoding='utf-8') as f2:
+    with open('config.txt') as f1, open('post.txt', encoding='utf-8') as f2, open('read.txt', encoding='utf-8') as f3:
         USERNAME, PASSWORD = f1.readlines()
         USERNAME = USERNAME.rstrip()
         POSTTEXT = f2.read()
+        READTEXT = f3.read()
 
     browser = webdriver.Chrome('chromedriver.exe')
 
@@ -24,25 +51,9 @@ if __name__ == '__main__':
     elem.send_keys(PASSWORD)
     elem.submit()
 
-    # Direct to the Create a Post View
-    browser.find_element_by_id('ember35').click()
+    # Make two Posts
+    post(POSTTEXT)
+    post(READTEXT)
 
-    # Enter the Post Content
-    t.sleep(2)
-    postarea = browser.find_element_by_class_name("ql-editor.ql-blank")
-    postarea.send_keys(POSTTEXT)
-
-    # Press the Post Button
-    t.sleep(2)
-    browser.find_element_by_xpath(
-        "//div[@class='share-box_actions']//span[@class='artdeco-button__text']").click()
-
-    # # Save the Screenshot (Just for checking the post)
-    t.sleep(2)
-    # browser.save_screenshot("TodayPost.png")
-    browser.quit()  # Close the browser
-    print('\n\nEverything went well! Hooray!')
-
-    # # Open up the png file for checking
-    # img = Image.open('TodayPost.png')
-    # img.show()
+    # Close the browser
+    browser.quit()
